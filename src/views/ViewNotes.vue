15672e227e3d0892ea586962cea92d3cff7cd2c9
Saba-Var <vartasashvili94@gmail.com>
2023-09-22 12:55:08 +0400

    chore: make notes list dynamic

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,5 +1,3 @@
-<script setup></script>
-
 <template>
   <div class="notes">
     <div class="card has-background-success-dark p-4 mb-5">
@@ -16,13 +14,15 @@
       </div>
     </div>
 
-    <div class="card mb-4" v-for="i in [1, 2, 3]" :key="i">
+    <div class="card mb-4" v-for="note in notes" :key="note.id">
       <div class="card-content">
         <div class="content">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima inventore quae
-          consequuntur quos asperiores optio temporibus illo harum reiciendis obcaecati explicabo
-          illum modi, doloribus repudiandae. Ipsum provident reprehenderit eius aliquam?
+          {{ note.content }}
         </div>
+        <p>
+          Created at:
+          <time :datetime="note.createdAt"> {{ note.createdAt }} </time>
+        </p>
       </div>
       <footer class="card-footer">
         <a href="#" class="card-footer-item">Edit</a>
@@ -31,3 +31,23 @@
     </div>
   </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+const notes = ref([
+  {
+    id: 1,
+    content:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima inventore quae consequuntur quos asperiores optio temporibus illo harum reiciendis obcaecati explicabo illum modi, doloribus repudiandae. Ipsum provident reprehenderit eius aliquam?',
+    createdAt: new Date().toDateString()
+  },
+
+  {
+    id: 2,
+    content:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima inventore quae consequuntur quos asperiores optio temporibus illo harum reiciendis obcaecati explicabo illum modi, doloribus repudiandae. Ipsum provident reprehenderit eius aliquam?',
+    createdAt: new Date().toDateString()
+  }
+])
+</script>
