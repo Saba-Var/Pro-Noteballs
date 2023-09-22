b347dec6f3e0a33282ffb3463bf037f98631ee39
Saba-Var <vartasashvili94@gmail.com>
2023-09-22 20:04:03 +0400

    feat: create 404 page

---
+++ src/views/NotFoundPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="not-found-container">
+    <h1>404 Page not found</h1>
+    <h2>it seems you're in the wrong page</h2>
+  </div>
+</template>
+
+<style scoped>
+.not-found-container {
+  position: fixed;
+  width: 20rem;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+h1 {
+  font-size: 2rem;
+  font-weight: 900;
+  color: #a12a21;
+  text-align: center;
+}
+
+h2 {
+  text-align: center;
+  color: gray;
+  font-size: 1rem;
+}
+</style>
--- src/views/index.js
+++ src/views/index.js
@@ -1,3 +1,4 @@
 export { default as ViewEditNote } from './ViewEditNote.vue'
+export { default as NotFoundPage } from './NotFoundPage.vue'
 export { default as ViewStats } from './ViewStats.vue'
 export { default as ViewNotes } from './ViewNotes.vue'
