c1a1c2b27f437bc79bace0cc1e0e37d8ab682356
Saba-Var <vartasashvili94@gmail.com>
2023-09-25 23:29:46 +0400

    chore: rename router file

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import { createApp } from 'vue'
 import '@/assets/main.css'
 
 import App from '@/App.vue'
-import router from '@/router'
+import { router } from '@/router'
 
 const pinia = createPinia()
 const app = createApp(App)
