bdf372d41b7e7eb0c134cbd899da0d433de1d9cc
Saba-Var <vartasashvili94@gmail.com>
2023-09-27 21:34:37 +0400

    feat: logout user

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -1,16 +1,14 @@
 import { createUserWithEmailAndPassword, signOut } from 'firebase/auth'
 import { useToast } from 'vue-toastification'
 import { firebaseAuth } from '@/services'
+import { useRouter } from 'vue-router'
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
 
 export const useAuthStore = defineStore('auth', () => {
   const toast = useToast()
-
-  const isRegistering = ref(false)
+  const router = useRouter()
 
   const registerUserHandler = async ({ email, password }) => {
-    isRegistering.value = true
     try {
       await createUserWithEmailAndPassword(firebaseAuth, email, password)
       toast.success('User registered successfully')
@@ -20,18 +18,17 @@ export const useAuthStore = defineStore('auth', () => {
       } else {
         toast.error('Something went wrong')
       }
-    } finally {
-      isRegistering.value = false
     }
   }
 
   const logoutHandler = async () => {
     try {
       await signOut(firebaseAuth)
+      router.push('/auth')
     } catch (error) {
       toast.error('Something went wrong')
     }
   }
 
-  return { registerUserHandler, isRegistering, logoutHandler }
+  return { registerUserHandler, logoutHandler }
 })
