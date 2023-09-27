0f9b13555326fd0e55846abbff187d84e9aed0c5
Saba-Var <vartasashvili94@gmail.com>
2023-09-27 22:08:08 +0400

    chore: remove unused response variable

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -44,7 +44,7 @@ export const useAuthStore = defineStore('auth', () => {
 
   const signInHandler = async ({ email, password }) => {
     try {
-      const response = await signInWithEmailAndPassword(firebaseAuth, email, password)
+      await signInWithEmailAndPassword(firebaseAuth, email, password)
       router.push('/')
     } catch (error) {
       if (error.code === 'auth/invalid-login-credentials') {
